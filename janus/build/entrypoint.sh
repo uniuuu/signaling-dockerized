7e37f8200db0e7cfd593897eb65f36993a2fe8f8
wh0ami <wh0ami@noreply.codeberg.org>
2021-04-19 23:19:33 +0200

    data from testserver

--- janus/build/entrypoint.sh
+++ janus/build/entrypoint.sh
@@ -5,9 +5,12 @@ cp /etc/janus/*.jcfg /janustmp/
 cp /etc/janus/janus.jcfg.template /janustmp/janus.jcfg
 chmod 640 /janustmp/*.jcfg
 
+# create files
+touch /usr/share/janus/streams/radio.alaw /usr/share/janus/streams/music.mulaw
+
 # fix parameters in config
 sed -i "s|TURNAPIKEY|${TURN_API_KEY}|g" /janustmp/janus.jcfg
-sed -i "s|SIGNALINGDOMAIN|${SIGNALING_HOSTNAME}|g" /janustmp/janus.jcfg
 sed -i "s|JANUSSECRET|${JANUS_API_KEY}|g" /janustmp/janus.jcfg
+sed -i "s|SIGNALINGDOMAIN|${SIGNALING_HOSTNAME}|g" /janustmp/janus.jcfg
 
 janus -C /janustmp/janus.jcfg
